0d3823b338ab0fbeadf4e0dccd73fb6dcc7921d2
ASUS <megabart57@gmail.com>
2022-01-22 13:33:54 +0300

    module

---
+++ client/src/main/java/com/geekbrains/App.java
@@ -0,0 +1,24 @@
+package com.geekbrains;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("start.fxml")));
+        primaryStage.setTitle("Perfect Cloud");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+
+    }
+}
---
+++ utils/src/main/java/com/geekbrains/Authentication.java
@@ -0,0 +1,25 @@
+package com.geekbrains;
+
+public class Authentication implements AbstractMessage{
+
+String login;
+String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Authentication(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public Commands getMessageType() {
+        return Commands.AUTHENTICATION;
+    }
+}
